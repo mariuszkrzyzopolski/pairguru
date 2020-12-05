dc882f21a401065a730122691be6a263492f4c5a
warhir <krzyzopolski.mariusz@gmail.com>
2020-12-05 14:50:54 +0100

    check if bracket is empty

--- pairguru/app/validators/title_brackets_validator.rb
+++ pairguru/app/validators/title_brackets_validator.rb
@@ -2,8 +2,11 @@ class TitleBracketsValidator < ActiveModel::Validator
   def validate(record)
     symbols = {"("=>")","{"=>"}","["=>"]"}
     brackets = []
-    record.title.each_char do |s|
-      brackets.push(s) if symbols.keys.include?(s)
+    record.title.each_char.with_index do |s, index|
+      if symbols.keys.include?(s)
+        record.errors.add(:title, "empty bracket") if symbols.values.include?(record.title[index+1])
+        brackets.push(s)
+      end
       record.errors.add(:title, "has invalid title") if symbols.key(s) && symbols.key(s) != brackets.pop
     end
     record.errors.add(:title, "has invalid title") unless brackets.empty?

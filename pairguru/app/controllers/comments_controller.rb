46ae40d2b9f4c7131afdbbf6640c38d23f9d1425
warhir <krzyzopolski.mariusz@gmail.com>
2020-12-05 10:38:51 +0100

    limit and destroy

--- pairguru/app/controllers/comments_controller.rb
+++ pairguru/app/controllers/comments_controller.rb
@@ -4,33 +4,39 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @comment = Comment.new(comment_params)
-    @comment.user_id = current_user.id
-    @comment.movie_id = params[:movie_id]
-
-    if have_comment? @comment.movie_id
+    if have_comment? params[:movie_id]
+      render :err
+    else
+      @comment = Comment.new(comment_params)
+      @comment.user_id = current_user.id
+      @comment.movie_id = params[:movie_id]
       @comment.save
       redirect_to movie_path(@comment.movie)
-    else
-      render :err and return
     end
 
   end
 
   def destroy
-    @comment = @movie.comments.find(params[:id])
+    @comment = Comment.find(params[:id])
+    @movie = find_movie params[:movie_id]
 
-    if @comment.user_id == current_user_id
+    if @comment.user_id == current_user.id
       @comment.destroy
     end
+    redirect_to movie_path(@movie)
   end
 
   def have_comment? movie_id
-    @movie = Movie.find(movie_id)
+    @movie = find_movie movie_id
+    return false if @movie.comments.all.empty?
     @movie.comments.reverse.each do |comment|
-      true if comment.user_id.eql? current_user.id
+      return true if comment.user_id == current_user.id
     end
     false
+  end
+
+  def find_movie(movie_id)
+    @movie = Movie.find(movie_id)
   end
 
   def comment_params
